c50530adc51a034bcbe9c8390d5f033ccb3caefa
Jacked <jaceb134@gmail.com>
2023-08-08 11:00:19 +0200

    messages dissplayed after login

--- src/app/message-list/message-list.component.ts
+++ src/app/message-list/message-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-
+import { take } from 'rxjs';
 import { Message } from '../shared/message';
 import { MessagesService } from '../messages.service';
 
@@ -12,7 +12,12 @@ export class MessageListComponent implements OnInit, OnDestroy {
   msgList: Message[];
   constructor(private messages: MessagesService) {}
   ngOnInit(): void {
-    this.msgList = this.messages.getMessages();
+    this.messages.msgDisplayer.subscribe({
+      next: (messages) => {
+        console.log('hello');
+        this.msgList = messages;
+      },
+    });
   }
   ngOnDestroy(): void {}
 }
--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Message } from './shared/message';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessagesService {
-  msgDisplayer = new Subject<Message[] | []>();
   messagesList: Message[] = [
     { author: 'Jack', message: 'Hello', date: new Date(Date.now()) },
     { author: 'Not Jack', message: 'Hi', date: new Date(Date.now()) },
     { author: 'Not Jack', message: 'Welcome', date: new Date(Date.now()) },
   ];
+  msgDisplayer = new BehaviorSubject<Message[] | []>(this.messagesList);
+
   getMessages() {
     this.msgDisplayer.next(this.messagesList.slice());
-    return this.messagesList.slice();
   }
   addMsg(data: Message) {
     this.msgDisplayer.next((this.messagesList = [...this.messagesList, data]));
