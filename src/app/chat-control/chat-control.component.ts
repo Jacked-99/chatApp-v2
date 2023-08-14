1f7b5814b922382fa139b6de9f66915fc9d07f9d
Jacked <jaceb134@gmail.com>
2023-08-14 17:14:42 +0200

    layout change and profile button

--- src/app/chat-control/chat-control.component.ts
+++ src/app/chat-control/chat-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MessagesService } from '../messages.service';
 import { Message } from '../shared/message';
@@ -11,8 +11,11 @@ import { take } from 'rxjs';
   styleUrls: ['./chat-control.component.css'],
 })
 export class ChatControlComponent implements OnInit {
+  @ViewChild('file', { static: true }) InputFile: ElementRef;
   user: any;
   msgForm: FormGroup;
+  filePreview = false;
+  fileUrl: string;
   constructor(
     private msgs: MessagesService,
     private authorService: UserService
@@ -36,7 +39,20 @@ export class ChatControlComponent implements OnInit {
     this.msgs.addMsg(data);
   }
   addEmoji(event) {
+    console.log(event);
     const newText = this.msgForm.value['msg'] + event.emoji.native;
     this.msgForm.setValue({ msg: newText });
+  }
+  onFileClick() {
+    let element = this.InputFile.nativeElement;
+    element.click();
+  }
+  onChange(event) {
+    this.fileUrl = URL.createObjectURL(event.target.files[0]);
+    this.filePreview = true;
+  }
+  closePreview() {
+    this.filePreview = false;
+    this.InputFile.nativeElement.value = '';
   }
 }
